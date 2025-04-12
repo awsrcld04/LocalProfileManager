bfc25220b4b5343bc7a1e7a36dbf53e8e69cf6ce
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-12 14:15:19 -0400

    Removed funcProgramRegistryTag from funcGetFuncCatchCode

--- LocalProfileManager.cs
+++ LocalProfileManager.cs
@@ -670,7 +670,6 @@ namespace LocalProfileManager
             dCatchTable.Add("funcPrintParameterSyntax", "f3");
             dCatchTable.Add("funcParseCmdArguments", "f4");
             dCatchTable.Add("funcProgramExecution", "f5");
-            dCatchTable.Add("funcProgramRegistryTag", "f6");
             dCatchTable.Add("funcCreateDSSearcher", "f7");
             dCatchTable.Add("funcCreatePrincipalContext", "f8");
             dCatchTable.Add("funcCheckNameExclusion", "f9");
